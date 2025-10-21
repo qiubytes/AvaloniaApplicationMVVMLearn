04a35c920187aa60e6d1f717a82df42b5df533c1
yuhandbcontext <yuhandbcontext@noreply.gitcode.com>
2025-10-21 21:10:42 +0800

    图片控件

--- AvaloniaApplicationMVVMLearn/ViewModels/WindowBindSelfViewModel.cs
+++ AvaloniaApplicationMVVMLearn/ViewModels/WindowBindSelfViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.ObjectModel;
+using System.IO;
+using Avalonia.Media.Imaging;
 using AvaloniaApplication1.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace AvaloniaApplication1.ViewModels;
@@ -8,6 +11,11 @@ public partial class WindowBindSelfViewModel : ViewModelBase
 {
     public ObservableCollection<People> Peoples { get; set; }
 
+    /// <summary>
+    /// 图片绑定
+    /// </summary>
+    [ObservableProperty]
+    public Bitmap _img;
     public WindowBindSelfViewModel()
     {
         Peoples = new ObservableCollection<People>()
@@ -15,6 +23,7 @@ public partial class WindowBindSelfViewModel : ViewModelBase
             new People() { Name = "张三", Des = "123" },
             new People() { Name = "李四", Des = "235" }
         };
+        _img = new Bitmap(new MemoryStream(File.ReadAllBytes(@"C:\Users\Lenovo\Downloads\微信图片_20250425143617.png")));
     }
 
     [RelayCommand]
