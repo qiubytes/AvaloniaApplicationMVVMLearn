2e4dd272bd56864d07bc96a5b7ab30e9584d44d7
yuhandbcontext <yuhandbcontext@noreply.gitcode.com>
2025-10-21 21:36:22 +0800

    异步按钮事件 显示图像

--- AvaloniaApplicationMVVMLearn/ViewModels/WindowBindSelfViewModel.cs
+++ AvaloniaApplicationMVVMLearn/ViewModels/WindowBindSelfViewModel.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Avalonia.Media.Imaging;
+using Avalonia.Threading;
 using AvaloniaApplication1.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -14,8 +18,8 @@ public partial class WindowBindSelfViewModel : ViewModelBase
     /// <summary>
     /// 图片绑定
     /// </summary>
-    [ObservableProperty]
-    public Bitmap _img;
+    [ObservableProperty] public Bitmap _img;
+
     public WindowBindSelfViewModel()
     {
         Peoples = new ObservableCollection<People>()
@@ -29,6 +33,34 @@ public partial class WindowBindSelfViewModel : ViewModelBase
     [RelayCommand]
     public void ItemButtonClick(object? obj)
     {
-        
+    }
+
+    /// <summary>
+    /// 异步绑定
+    /// </summary>
+    public Task<string> MyText => GetTextAsync();
+
+    public async Task<string> GetTextAsync()
+    {
+        await Task.Delay(3000);
+        return "Hello World";
+    }
+
+    [RelayCommand]
+    public async Task GetImageBtnClick()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage responseMessage =
+                await client.GetAsync(new Uri("https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"));
+            responseMessage.EnsureSuccessStatusCode();
+            Stream stream = await responseMessage.Content.ReadAsStreamAsync();
+            // _img = new Bitmap(stream);
+            Dispatcher.UIThread.Post(() =>
+            {
+                Img?.Dispose();//Img 会自动引发 PropertyChanged 事件，_img则不能
+                Img = new Bitmap(stream);
+            });
+        }
     }
 }
